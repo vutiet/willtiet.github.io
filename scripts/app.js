dd7b378a6a35e21e1b77e2321a391bdf191b862a
Vu Tiet <vutiet@gmail.com>
2018-07-08 14:46:02 +0800

    update content

---
+++ scripts/app.js
@@ -0,0 +1,22 @@
+$(function () {
+    $('#aboutNav').on('click', e => {
+        e.preventDefault();
+        $('body, html').animate({
+            scrollTop: $('#about').offset().top - $('.header').innerHeight()
+        }, 500);
+    });
+
+    $('#portfolioNav').on('click', e => {
+        e.preventDefault();
+        $('body, html').animate({
+            scrollTop: $('#portfolio').offset().top - $('.header').innerHeight()
+        }, 500);
+    });
+
+    $('#contactNav').on('click', e => {
+        e.preventDefault();
+        $('body, html').animate({
+            scrollTop: $('#contact').offset().top - $('.header').innerHeight()
+        }, 500);
+    });
+});
